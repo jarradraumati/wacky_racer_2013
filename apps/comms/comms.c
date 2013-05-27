3b98b895dba8a4f4f91ac01665d66d16da890bb7
Nick Wyatt <nick@hackbook.(none)>
2013-05-27 14:05:38 +1200

    Commit comms

--- apps/comms/comms.c
+++ apps/comms/comms.c
@@ -10,19 +10,20 @@
 #include "bluetooth.h"
 #include <stdio.h>
 #include "sys.h"
-#include "pacer.h"
 #include "string.h"
-
+#include "delay.h"
 #include "kernel.h"
+#include "ibp.h"
 
 /* Globals */
+uint8_t ch;
 
 /* ID of every task. Global so each task
  * may interact with other tasks.
  *
  * Add a new value whenever you register a new task
  */
-enum {LED_FLASH, LED1_FLASH, LED2_FLASH};
+enum {BLUETOOTH_COMMS_TASK, ACTION_COMMANDS_TASK};
 
 /* Commands sent from the comms board over bluetooth */
 enum {CMD_NONE = 0x00, CMD_LEFT = 0x30, CMD_RIGHT, CMD_FORWARD, CMD_BACK,
@@ -47,18 +48,77 @@ static const i2c_slave_cfg_t i2c_motor_cfg =
     .id = 0x16,
 };
 
+i2c_t i2c_camera;
+i2c_t i2c_motor;
+
 typedef struct state_struct
 {
         uint32_t led;
         uint8_t on;
 } state_t;
 
-void led_flash (void *data)
+void 
+led_flash (void *data)
 {
-        state_t *state = data;
-        pio_output_toggle (state->led);
+    state_t *state = data;
+    pio_output_toggle (state->led);
 }
 
+void
+bluetooth_comms_task (void *data)
+{
+    bluetooth_t btconn = data;
+    // Check if there is anything to be read
+    if (bluetooth_read_ready_p (btconn))
+        ch = bluetooth_receive_command (btconn);
+
+    // Wait until we can send it back
+    //while(!bluetooth_write_ready_p(btconn)) continue;
+    
+    /* Echo it back to the sender. */
+    //bluetooth_send_command (btconn, ch);
+
+
+}
+
+void
+action_commands_task (void *data)
+{
+    switch (ch) 
+    {
+        case CMD_LEFT:
+            pio_output_toggle (LED_GREEN_PIO);
+            break;
+        case CMD_RIGHT:
+            pio_output_toggle (LED_ORANGE_PIO);
+            break;
+        case CMD_FORWARD:
+            pio_output_toggle (LED_RED_PIO);
+            break;
+        case CMD_BACK:
+            pio_output_toggle (LED_BLUE_PIO);
+            break;
+        case CMD_STOP:
+            break;
+    };
+    if (ch != CMD_NONE)
+    {
+        // Send interrupt to slaves
+        pio_config_set (i2c_bus_cfg.scl, PIO_OUTPUT_LOW);
+        DELAY_US (3);
+        // Set clock back ready for i2c
+        pio_config_set (i2c_bus_cfg.scl, PIO_PULLUP);
+        DELAY_US (20);
+        i2c_master_addr_write (i2c_camera, COMMS_COMMAND, 1, &ch, 1);
+        
+        // Ensure clock is left in a pullup state
+        pio_config_set (i2c_bus_cfg.scl, PIO_PULLUP);
+    }
+        
+    
+
+    ch = CMD_NONE;
+}
 
 
 int
@@ -73,50 +133,24 @@ main ()
     pio_config_set (LED_RED_PIO, PIO_OUTPUT_HIGH);
     pio_config_set (LED_BLUE_PIO, PIO_OUTPUT_HIGH);
 
-    /* Initialise i2c */
-    i2c_t i2c_motor;
-    i2c_t i2c_camera;
+    int empty = 0;
     
-    uint8_t ch;
 
+    
+    /* Initialise i2c */
     i2c_motor = i2c_master_init (&i2c_bus_cfg, &i2c_motor_cfg);
     i2c_camera = i2c_master_init (&i2c_bus_cfg, &i2c_camera_cfg);
 
     /* Initialise bluetooth */
-    usart_t btconn = bluetooth_init(115200);
-    //kernel_init ();
+    bluetooth_t btconn = bluetooth_init(115200);
+    kernel_init ();
+    
+    
+    kernel_taskRegister (bluetooth_comms_task, BLUETOOTH_COMMS_TASK, btconn, 100);
+    kernel_taskRegister (action_commands_task, ACTION_COMMANDS_TASK, &led1_data, 100);
+    
+    kernel_start ();
+    
+    return 0;
    
-    pacer_init (PACER_RATE);
-
-    while (1)
-    {
-        
-        /* Wait for a character to be sent. */
-        ch = bluetooth_receive_command (btconn);
-        
-        /* Wait until we are able to transmit it back. */
-        while(!bluetooth_write_ready_p(btconn)) continue;
-        
-        switch (ch) 
-        {
-            case CMD_LEFT:
-                pio_output_toggle (LED_GREEN_PIO);
-                break;
-            case CMD_RIGHT:
-                pio_output_toggle (LED_ORANGE_PIO);
-                break;
-            case CMD_FORWARD:
-                pio_output_toggle (LED_RED_PIO);
-                break;
-            case CMD_BACK:
-                pio_output_toggle (LED_BLUE_PIO);
-                break;
-            case CMD_STOP:
-                break;
-                // Do stop stuff
-        };
-        
-        /* Echo it back to the sender. */
-        bluetooth_send_command (btconn, ch);
-    }
 }
